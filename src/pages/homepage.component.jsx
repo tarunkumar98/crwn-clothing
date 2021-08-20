91f438085cbe87b02683df520a3c3ae1b039f6f1
tarunkumar98 <chikkalatarunkumar1999@gmail.com>
2021-08-20 23:03:58 +0530

    broken the homepage and add the styling to the homepages

---
+++ src/pages/homepage.component.jsx
@@ -0,0 +1,10 @@
+import Directory from '../components/directory/directory.component';
+import './homepage.style.scss';
+
+export const HomePage=()=>{
+   return <div className='homepage'>
+        <Directory/>
+    </div>  
+}
+
+export default HomePage;
