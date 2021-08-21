c7da73ffbe3b61c36e61b771c1b75c415ba54718
tarunkumar98 <chikkalatarunkumar1999@gmail.com>
2021-08-21 13:03:51 +0530

    adding shoping page to our app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Switch,Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 const HatsPage=()=>(
   <div>
@@ -13,7 +14,8 @@ function App() {
     <div>
       <Switch>
         <Route exact path='/' component={HomePage}/>
-        <Route path='/shop/hats' component={HatsPage}/>
+        <Route exact path='/shop/hats' component={HatsPage}/>
+        <Route path='/shop' component={ShopPage}/>
       </Switch>
     </div>
   );
